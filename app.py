b132e7be098f366104dbc1920f32ebc2e0db6960
danikstarik <dankudrin@gmail.com>
2017-08-13 20:12:58 +0400

    added nginx and uwsgi ini's, renamed entry point

--- app.py
+++ app.py
@@ -33,18 +33,7 @@ def home():  # pragma: no cover
 def send_js(path):
     return send_from_directory('static/', path)
 
-# def get_resource(path):  # pragma: no cover
-#     mimetypes = {
-#         ".css": "text/css",
-#         ".html": "text/html",
-#         ".js": "application/javascript",
-#     }
-#     complete_path = os.path.join(root_dir(), path)
-#     ext = os.path.splitext(path)[1]
-#     mimetype = mimetypes.get(ext, "text/html")
-#     content = get_file(complete_path)
-#    
 
 
 if __name__ == '__main__':  # pragma: no cover
-    app.run(host='0.0.0.0',debug = True)
+    app.run(debug = True)
